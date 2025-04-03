592c89aa0c2342e0c79600b0bf38e3206def0816
davis7555 <davisjohn7555@gmail.com>
2025-04-03 18:10:42 +0200

    preparing docker

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -45,15 +45,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function suggestion(): BelongsTo
-    {
-        return $this->belongsTo(Suggestion::class);
-    }
-
-    public function question(): BelongsTo
-    {
-        return $this->belongsTo(Question::class);
     }
 }
